e3ed2f9b2a9223611bc92675a1dcfadafdd78cd2
Glowman554 <glowman434@gmail.com>
2024-07-15 23:27:46 +0200

    added request caching / deduplication

--- client/token.ts
+++ client/token.ts
@@ -3,16 +3,20 @@ import { trpc } from "../server/trpc/client.ts";
 import { QueryState, useQuery, withQuery } from "./helper.ts";
 
 export function useToken(q: QueryState) {
-    const query = useQuery(async () => {
-        const token = localStorage.getItem("token");
-        if (token) {
-            const ok = await trpc.users.test.query(token);
-            if (ok) {
-                return token;
+    const query = useQuery(
+        async () => {
+            const token = localStorage.getItem("token");
+            if (token) {
+                const ok = await trpc.users.test.query(token);
+                if (ok) {
+                    return token;
+                }
             }
-        }
-        return undefined;
-    }, q);
+            return undefined;
+        },
+        q,
+        "token",
+    );
 
     return query;
 }
@@ -28,6 +32,7 @@ export function useIsEditor(
                 () => trpc.users.isEditor.query(token),
                 q,
                 setIsEditor,
+                "isEditor",
             );
         }
     }, [token]);
